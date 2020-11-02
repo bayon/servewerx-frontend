1bbefa173d3363642e28351ee9cab45d8f3d57a1
Bayon Forte <bayon@forteworks.com>
2020-11-02 10:15:24 -0500

    use react hooks to make http request to backend

--- servewerx-frontend/src/App.js
+++ servewerx-frontend/src/App.js
@@ -1,7 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState,useEffect } from 'react';
+
+
+
+
 
 function App() {
+
+  const [data, setData] = useState(null);
+
+   
+  useEffect(() => {
+    // GET request using fetch inside useEffect React hook
+    fetch('http://localhost:3003/url')
+        .then(response => response.json())
+        .then(data => setData(data));
+
+  // empty dependency array means this effect will only run once (like componentDidMount in classes)
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +35,7 @@ function App() {
         >
           Learn React
         </a>
+        <div>{data}</div>
       </header>
     </div>
   );
