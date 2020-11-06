767c8f4e3e2ef7013c49078d65273e650b79fd1e
Bayon Forte <bayon@forteworks.com>
2020-11-06 10:20:19 -0500

    integrate the UI login work into servewerx

--- servewerx-frontend/src/App.js
+++ servewerx-frontend/src/App.js
@@ -1,7 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useState,useEffect } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import PrivateRoute from "./PrivateRoute";
+import Home from "./pages/Home";
+import Admin from "./pages/Admin";
+import { AuthContext } from "./context/auth";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
+function App(props) {
+  const [authTokens, setAuthTokens] = useState();
+  /*
+  const [data, setData] = useState([]);
+  const api_prod = "http://67.205.153.202:3000/api/vTest/junk"
+  const api_dev = "http://localhost:3000/api/vTest/junk"
+    useEffect(() => {
+    // GET request using fetch inside useEffect React hook
+    // Production:    http://67.205.153.202:3000/url
+    // Development:   http://localhost:3000/url
+    fetch(api_prod)
+        .then(response => response.json())
+        .then(data => setData(data));
+
+  // empty dependency array means this effect will only run once (like componentDidMount in classes)
+  }, []);
+
+  var display = JSON.stringify(data) // TO DISPLAY:  <div>{display}</div>
+  */
+
+  const setTokens = (data) => {
+    localStorage.setItem("tokens", JSON.stringify(data));
+    setAuthTokens(data);
+  };
+
+  return (
+    <AuthContext.Provider value={{ authTokens, setAuthTokens: setTokens }}>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home Page</Link>
+            </li>
+            <li>
+              <Link to="/admin">Admin Page</Link>
+            </li>
+          </ul>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <PrivateRoute path="/admin" component={Admin} />
+        </div>
+      </Router>
+    </AuthContext.Provider>
+  );
+}
+
+export default App;
+
+/*
 function App() {
 
   const [data, setData] = useState([]);
@@ -42,3 +99,4 @@ function App() {
 }
 
 export default App;
+*/
---
+++ servewerx-frontend/src/pages/Login.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+import axios from 'axios';
+import logoImg from "../img/logo.jpg";
+import { Card, Logo, Form, Input, Button, Error } from "../components/AuthForms";
+import { useAuth } from "../context/auth";
+
+function Login(props) {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const { setAuthTokens } = useAuth();
+  //const referer = props.location.state.referer || '/';
+  const referer = props.location || '/';
+
+
+  function postLogin() {
+    console.log('START: postLogin')
+ 
+    axios.post("http://localhost:4000/api/vTest/auth/login", {
+      userName,
+      password
+      
+    }).then(result => {
+      console.log("LOGIN : THEN RESULT ")
+      if (result.status === 200) {
+        console.log("LOGIN status code 200")
+        console.log("result:",result);
+        console.log("LOGIN RESULT DATA:",result.data)
+        setAuthTokens(result.data);
+        setLoggedIn(true);
+      } else {
+        console.log("LOGIN result.status !=== 200 ")
+        setLoggedIn(false);
+        setIsError(true);
+      }
+    }).catch(e => {
+      console.log("LOGIN catch axios error e:",e)
+      setLoggedIn(false);
+      setIsError(true);
+    });
+  }
+
+  if (isLoggedIn) {
+    console.log("LOGIN isLoggedIN = TRUE")
+    return <Redirect to={referer} />;
+  }
+
+  return (
+    <Card>
+      <Logo src={logoImg} />
+      <Form>
+        <Input
+          type="username"
+          value={userName}
+          onChange={e => {
+            setUserName(e.target.value);
+          }}
+          placeholder="email"
+        />
+        <Input
+          type="password"
+          value={password}
+          onChange={e => {
+            setPassword(e.target.value);
+          }}
+          placeholder="password"
+        />
+        <Button onClick={postLogin}>Sign In</Button>
+      </Form>
+      <Link to="/signup">Don't have an account?</Link>
+        { isError &&<Error>The username or password provided were incorrect!</Error> }
+    </Card>
+  );
+}
+
+export default Login;
